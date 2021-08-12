40efa86b99936d9a608007db294b5a90a97c715f
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-12 16:40:39 +0530

    Added functionality to view employee payroll details from the local storage

--- js/home.js
+++ js/home.js
@@ -1,24 +1,34 @@
+let employeePayRollList;
 window.addEventListener('DOMContentLoaded', () => {
+    employeePayRollList = getEmployeePayRollDataFromStorage();
+    document.querySelector('.emp-count').textContent = employeePayRollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
+
+const getEmployeePayRollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayRollList') ?
+        JSON.parse(localStorage.getItem('EmployeePayRollList')) : [];
+};
 
 const createInnerHtml = () => {
     const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th>" +
         "<th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
+    if (employeePayRollList.length == 0)
+        return;
     let innerHtml = `${headerHtml}`;
-    let employeePayRollList = createEmployeePayRollJSON();
     for (const employeePayRollData of employeePayRollList) {
         innerHtml = `${innerHtml}
             <tr>
-                <td class="profile-image"><img class="profile" src="${employeePayRollData.image}" alt=""></td>
-                <td>${employeePayRollData.name}</td>
-                <td>${employeePayRollData.gender}</td>
-                <td>${getDeptHtml(employeePayRollData.department)}</td>
-                <td>${employeePayRollData.salary}</td>
-                <td>${employeePayRollData.startDate}</td>
+                <td class="profile-image"><img class="profile" src="${employeePayRollData._image}" alt=""></td>
+                <td>${employeePayRollData._name}</td>
+                <td>${employeePayRollData._gender}</td>
+                <td>${getDeptHtml(employeePayRollData._department)}</td>
+                <td>${employeePayRollData._salary}</td>
+                <td>${getDateInFormat(employeePayRollData._startDate)}</td>
                 <td>
-                    <img name="${employeePayRollData.id}" src="../assets/icons/delete-black-18dp.svg" alt="delete" onclick="remove(this)">
-                    <img name="${employeePayRollData.id}" src="../assets/icons/create-black-18dp.svg" alt="edit" onclick="update(this)">
+                    <img id="${employeePayRollData._id}" src="../assets/icons/delete-black-18dp.svg" alt="delete" onclick="remove(this)">
+                    <img id="${employeePayRollData._id}" src="../assets/icons/create-black-18dp.svg" alt="edit" onclick="update(this)">
                 </td>
             </tr>
             `;
@@ -33,34 +43,11 @@ const getDeptHtml = (deptList) => {
     return deptHtml;
 };
 
-const createEmployeePayRollJSON = () => {
-    let employeePayRollListLocal = [
-        {
-            name: 'Edogava Conan',
-            gender: 'Male',
-            department: [
-                'Engineering',
-                'Sales'
-            ],
-            salary: '500000',
-            startDate: '29 Oct 2019',
-            note: '',
-            id: 'new Date().getTime()',
-            image: '../assets/profile-images/Ellipse -2.png'
-        },
-        {
-            name: 'Light Yagami',
-            gender: 'Male',
-            department: [
-                'HR',
-                'Sales'
-            ],
-            salary: '450000',
-            startDate: '29 May 2017',
-            note: '',
-            id: new Date().getTime() + 1,
-            image: '../assets/profile-images/Ellipse -3.png'
-        }
-    ];
-    return employeePayRollListLocal;
+const getDateInFormat = (startDate) => {
+    let date = new Date(startDate);
+    const options = {
+        year: 'numeric', month: 'short', day: 'numeric'
+    };
+    const empDate = date == undefined ? "undefined" : date.toLocaleDateString("en-IN", options);
+    return empDate;
 };
