a16079b3120576b065f203fc7dc8c50476e201cc
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-16 16:13:47 +0530

    Added : the validation for name and start date

--- js/utility.js
+++ js/utility.js
@@ -6,3 +6,18 @@ const getDateInFormat = (startDate) => {
     const empDate = date == undefined ? "undefined" : date.toLocaleDateString("en-IN", options);
     return empDate;
 };
+
+const checkName = (name) => {
+    let regexName = RegExp('^[A-Z][A-Za-z ]{2,}$');
+    if (!regexName.test(name)) throw 'Name is Invalid!';
+};
+
+const checkStartDate = (startDate) => {
+    let now = new Date();
+    let date = new Date(startDate);
+    if (date > new Date())
+        throw 'Start Date is a Future Date!';
+    var diff = Math.abs(now.getTime() - date.getTime());
+    if (diff / (1000 * 60 * 60 * 24) > 30)
+        throw 'Start Date is beyond 30 Days of current Date!';
+};
